ec15177eb3f5df88b34071b189ee4f78a8928e79
ulkimion <101999652+ulkimion@users.noreply.github.com>
2024-12-11 17:58:44 -0300

    Special events funcional

---
+++ Assets/Scripts/SpecialEventButton.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class SpecialEventButton : MonoBehaviour, IPointerClickHandler
+{
+    public TextMeshProUGUI optionText;
+    public CurrentRun currentRun;
+    public ExtraExp extraExp;
+    public string option;
+    private bool isClicked = false;
+    void Start()
+    {
+        optionText.text = option;
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Debug.Log("Se llego hasta aqui");
+        if (!isClicked)
+        {
+            if (option == "Gain 50 Gold, Fight a combat")
+            {
+                currentRun.money = currentRun.money + 50;
+                SceneManager.LoadScene("BattleScene");
+            }
+            else if (option == "Gain 5 Exp for your Bullets")
+            {
+                extraExp.extraExp = 4;
+                SceneManager.LoadScene("BulletExp");
+            }
+            else if (option == "Heal for 15 HP")
+            {
+                if (currentRun.currentHP + 15 > currentRun.maxHP)
+                {
+                    currentRun.currentHP = currentRun.maxHP;
+                }
+                else
+                {
+                    currentRun.currentHP = currentRun.currentHP + 15;
+                }
+
+                SceneManager.LoadScene("MainMenu");
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/SpecialEventsOptions.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpecialEventsOptions : MonoBehaviour
+{
+    public GameObject option1;
+    public GameObject option2;
+    public GameObject option3;
+
+    private void Start()
+    {
+        SpecialEventButton specialEventButton1 = option1.GetComponent<SpecialEventButton>();
+        specialEventButton1.option = "Gain 50 Gold, Fight a combat";
+        SpecialEventButton specialEventButton2 = option2.GetComponent<SpecialEventButton>();
+        specialEventButton2.option = "Gain 5 Exp for your Bullets";
+        SpecialEventButton specialEventButton3 = option3.GetComponent<SpecialEventButton>();
+        specialEventButton3.option = "Heal for 15 HP";
+    }
+}
